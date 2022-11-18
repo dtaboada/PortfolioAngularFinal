284b2972e9d5f2b2fa9785bd6c625fb0b45a775c
Diego Taboada <dsp_diego@hotmail.com>
2022-11-17 21:26:13 -0300

    Primer commit, Portfolio con Angulas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      
+      {path:'about',component:AboutComponent},
+      {path:'experiencia',component:ExperienciaComponent},
+      {path:'educacion',component: EducacionComponent},
+      {path:'skills',component: SkillsComponent},
+      {path:'proyectos',component: ProyectosComponent},
+      {path:'**',component:AboutComponent}
+  ])
+
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
