e140d848cfa79a6d598e2d456700f59ad5f4542c
Diego Taboada <dsp_diego@hotmail.com>
2022-12-09 04:15:12 -0300

    acomodado con errores

--- src/app/serviciosPruebaJson/portfolio.service.ts
+++ src/app/serviciosPruebaJson/portfolio.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/internal/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PortfolioService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PortfolioService {
 
-  constructor( private http:HttpClient ) { }
+//   constructor( private http:HttpClient ) { }
 
-  obtenerDatos():Observable<any> {
-    return this.http.get('../assets/datosPrueba/persona.json')
-  }
-}
+//   obtenerDatos():Observable<any> {
+//     return this.http.get('../assets/datosPrueba/persona.json')
+//   }
+// }
